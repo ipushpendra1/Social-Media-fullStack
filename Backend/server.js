ff53f64f563a155e5f64322ea1ff4849e59dc4b5
pushpendra <pushpendra090804@gmail.com>
2025-08-05 23:39:53 +0530

    Socket.io  installation and initialization in server page

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,31 @@
 import app from "./src/app.js"
 import connectDB from "./src/db/db.js"
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+      });
+  });
+
+  
+
+
+
+
+
 
 
 connectDB();
-app.listen(3000,()=>{
+
+httpServer.listen(3000,()=>{
     console.log("server is running on port 3000");
     
 })
